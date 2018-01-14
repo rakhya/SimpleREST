bce14820421ceb3681b127fcee06b4d33038037c
rakhya <pravarakhya.chilukuri@gmail.com>
2018-01-14 03:17:59 -0500

    XML data returned

--- SimpleREST/src/main/java/com/resources/FirstResource.java
+++ SimpleREST/src/main/java/com/resources/FirstResource.java
@@ -1,12 +1,18 @@
 package com.resources;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.models.Student;
+import com.services.StudentService;
+
 @Path("/")
 public class FirstResource {
+	StudentService ss = new StudentService();
 	
 	@Path("/resource1")
 	@GET
@@ -28,4 +34,13 @@ public class FirstResource {
 	public String getMessage3() {
 		return "3 , Hello, REST!";
 	}
+	
+	@Path("students")
+	@GET
+	@Produces({MediaType.APPLICATION_XML})
+	public List<Student> getStudents(){
+		ss.addStudent(1, "rakhya", 10);
+		ss.addStudent(2, "harika", 10);
+		return ss.getStudents();
+	} 
 }
