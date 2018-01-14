a88472744a2f66c21965c634444e324af7646fdd
rakhya <pravarakhya.chilukuri@gmail.com>
2018-01-14 02:29:48 -0500

    Second Commit

--- SimpleREST/src/main/java/com/resources/FirstResource.java
+++ SimpleREST/src/main/java/com/resources/FirstResource.java
@@ -5,12 +5,27 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/myresource")
+@Path("/")
 public class FirstResource {
 	
+	@Path("/resource1")
 	@GET
 	@Produces({MediaType.APPLICATION_JSON})
-	public String getMessage() {
+	public String getMessage1() {
 		return "1 , Hello, REST!";
+	}
+	
+	@Path("/resource2")
+	@GET
+	@Produces({MediaType.APPLICATION_JSON})
+	public String getMessage2() {
+		return "2 , Hello, REST!";
+	}
+	
+	@Path("/resource3")
+	@GET
+	@Produces({MediaType.APPLICATION_JSON})
+	public String getMessage3() {
+		return "3 , Hello, REST!";
 	}
 }
